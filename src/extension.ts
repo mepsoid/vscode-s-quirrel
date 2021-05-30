89c6f5e33fa8cb0f8063318b3a56fa9fda4b03a9
a.vinokurov <a.vinokurov@gaijin.team>
2021-05-30 17:39:16 +0300

    Added auto completion for common table key-value combinations

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,22 @@
 import * as vs from 'vscode';
 
 import { SubstitutePathByFile } from './SubstitutePathByFile';
+import { SubstituteValueByKey } from './SubstituteValueByKey';
 import openFileByPath from './openFileByPath';
 import runDocumentCode from './runDocumentCode';
 import checkSyntaxOnSave from './checkSyntaxOnSave';
 import clearDiagsOnClose from './clearDiagsOnClose';
 
+const DOCUMENT: vs.DocumentSelector = { language: 'squirrel', scheme: 'file' };
+
 export function activate(context: vs.ExtensionContext) {
-  const DOCUMENT: vs.DocumentSelector = { language: 'squirrel', scheme: 'file' };
   const completePath: vs.Disposable = vs.languages.registerCompletionItemProvider(
     DOCUMENT, new SubstitutePathByFile(), '"', '/', '\\');
   context.subscriptions.push(completePath);
+
+  const completeValue: vs.Disposable = vs.languages.registerCompletionItemProvider(
+    DOCUMENT, new SubstituteValueByKey(), '=');
+  context.subscriptions.push(completeValue);
 
   const commandOpenByPath: vs.Disposable = vs.commands.registerCommand(
     'squirrel.editor.action.openModule', openFileByPath);
